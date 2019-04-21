06de465d1f15951d4f2202e59a215080b5fafb71
WengDog <43691689+WengDog@users.noreply.github.com>
2019-04-22 00:48:00 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,13 +4,12 @@ from REGEX import REGEX
 import loadqna
 from sanitize import sanitize
 
-#load database pertanyaan dan jawaban
+# load database pertanyaan dan jawaban
 kamus, lisp = loadqna.load("qna")
 
-
-
-
-
+# list untuk menyimpan pertanyaan yang mirip
+listPertanyaan = []
+listKataSama = []
 
 #terima input user dan bersihkan dari dari stop word dan 
 #ganti kata kata di dalamnya dengan sinonim yang terdapat 
@@ -21,7 +20,45 @@ pertanyaan = sanitize(raw_input())
 algo = raw_input()
 
 if(algo == "KMP"):
-	
+	# cek keseluruhan kalimat terlebih dahulu
+	for i in range(len(lisp)):
+		idx = KMP.KMP(lisp[i], pertanyaan)
+		if(idx != -1):
+			listPertanyaan.append(lisp[i])
+
+	# cek kecocokan berdasarkan kata per kata dari input user jika tidak ada kata yang sama persis
+	if(len(listPertanyaan) == 0):
+		kalimat = pertanyaan.split(' ')
+		prevIdx = -1
+		for i in range(len(lisp)):
+			for kata in kalimat:
+				idx = KMP.KMP(lisp[i], kata)
+				if (idx > prevIdx):
+					listKataSama.append(kata)
+					prevIdx = idx
+
+			# hitung jumlah kata yang sama
+			count = 0
+			for kata in listKataSama:
+				count += len(kata)
+			
+			count += len(kalimat) - 1 
+			# apakah kata sudah mirip >= 80% jika ya masukkan ke list pertanyaan
+			persentase = count/len(p[i]) * 100
+			if(persentase >= 80):
+				listPertanyaan.append(p[i])
+
+	# keluarkan jawaban dari bot
+	if(len(listPertanyaan) == 0):
+		print("Can you ask other question ?")
+	else:
+		if(len(listPertanyaan) == 1):
+			print(k[listPertanyaan[0]])
+		# lebih dari 1 yang mirip keluarkan jawaban random dari listPertanyaan yang terpilih
+		else:
+			idx = random.randint(0, len(listPertanyaan))
+			print("xesbot : " + k[listPertanyaan[idx]])
+
 elif(algo == "BM"):
 	result = -1
 	i = 0
@@ -39,7 +76,7 @@ elif(algo == "BM"):
 				i += 1
 			result = kamus[i]
 		else:
-			print("Can you ask other question.")
+			print("Can you ask other question ?")
 	else:
 		result = kamus[i]
 
