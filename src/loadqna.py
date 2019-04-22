e13e88a27de9dc35d7de2d2f1618c3bf6335df01
WengDog <43691689+WengDog@users.noreply.github.com>
2019-04-22 09:11:22 +0700

    update

--- src/loadqna.py
+++ src/loadqna.py
@@ -18,52 +18,5 @@ def load(filename):
 	return kamus, pertanyaan
 
 
-# k, p = load("qna")
-# q = []
-# print("Halo ini xesbot, Apakah ada yang bisa dibantu ?")
-# q = input("You : ").lower()
-
-# kalimat = q.split(' ')
-# listKataSama = []
-# listPertanyaan = []
-
-# # cek keseluruhan kalimat
-# for i in range(len(p)):
-# 	idx = algorithm.KMP(p[i], q)
-# 	if (idx != -1):
-# 		listPertanyaan.append(p[i])
-
-# # cek kecocokan berdasarkan kata per kata dari input user jika tidak ada kata yang sama persis
-# if(len(listPertanyaan) == 0):
-# 	prevIdx = -1
-# 	for i in range(len(p)):
-# 		for kata in kalimat:
-# 			idx = algorithm.KMP(p[i], kata)
-# 			if (idx > prevIdx):
-# 				listKataSama.append(kata)
-# 				prevIdx = idx
-
-# 		# hitung jumlah kata yang sama
-# 		count = 0
-# 		for kata in listKataSama:
-# 			count += len(kata)
-		
-# 		count += len(kalimat) - 1 
-# 		# apakah kata sudah mirip >= 80% jika ya masukkan ke list pertanyaan
-# 		persentase = count/len(p[i]) * 100
-# 		if(persentase >= 80):
-# 			listPertanyaan.append(p[i])
-
-# # keluarkan jawaban dari bot
-# if(len(listPertanyaan) == 0):
-# 	print("xesbot : Apakah ada pertanyaan lain ?")
-# else:
-# 	if(len(listPertanyaan) == 1):
-# 		print("xesbot : " +  k[listPertanyaan[0]])
-# 	# lebih dari 1 yang mirip keluarkan jawaban random dari listPertanyaan yang terpilih
-# 	else:
-# 		idx = random.randint(0, len(listPertanyaan))
-# 		print("xesbot : " + k[listPertanyaan[idx]])
-
 
 
--- src/main.py
+++ src/main.py
@@ -29,55 +29,57 @@ def home(page=None):
 #kmp page
 @app.route('/kmp', methods=['POST'])
 def getkmp():
-	# list untuk menyimpan pertanyaan yang mirip
-	listPertanyaan = []
-	listKataSama = []
-	result = ''
-
 	#terima input user dan bersihkan dari dari stop word dan 
 	#ganti kata kata di dalamnya dengan sinonim yang terdapat 
 	#dalam database
 	pertanyaan = sanitize(request.form['text']).lower()
-	# cek keseluruhan kalimat terlebih dahulu
-	for i in range(len(lisp)):
-		idx = KMP.KMP(lisp[i], pertanyaan)
-		if(idx != -1):
-			listPertanyaan.append(lisp[i])
-
-	# cek kecocokan berdasarkan kata per kata dari input user jika tidak ada kata yang sama persis
-	if(len(listPertanyaan) == 0):
-		kalimat = pertanyaan.split(' ')
-		prevIdx = -1
+	if (len(pertanyaan) != 0):
+		# list untuk menyimpan pertanyaan yang mirip
+		listPertanyaan = []
+		listKataSama = []
+		result = ''
+		# cek keseluruhan kalimat terlebih dahulu
 		for i in range(len(lisp)):
-			for kata in kalimat:
-				idx = KMP.KMP(lisp[i], kata)
-				if (idx > prevIdx):
-					listKataSama.append(kata)
-					prevIdx = idx
-
-			# hitung jumlah kata yang sama
-			count = 0
-			for kata in listKataSama:
-				count += len(kata)
-			
-			count += len(kalimat) - 1 
-			# apakah kata sudah mirip >= 80% jika ya masukkan ke list pertanyaan
-			persentase = count/len(lisp[i]) * 100
-			if(persentase >= 80):
+			idx = KMP.KMP(lisp[i], pertanyaan)
+			if(idx != -1):
 				listPertanyaan.append(lisp[i])
 
-	# keluarkan jawaban dari bot
-	if(len(listPertanyaan) == 0):
-		# print("Can you ask other question ?")
-		result = "Can you ask another question?"
+		# cek kecocokan berdasarkan kata per kata dari input user jika tidak ada kata yang sama persis
+		if(len(listPertanyaan) == 0):
+			kalimat = pertanyaan.split(' ')
+			prevIdx = -1
+			for i in range(len(lisp)):
+				for kata in kalimat:
+					idx = KMP.KMP(lisp[i], kata)
+					if (idx > prevIdx):
+						listKataSama.append(kata)
+						prevIdx = idx
+
+				# hitung jumlah kata yang sama
+				count = 0
+				for kata in listKataSama:
+					count += len(kata)
+				
+				count += len(kalimat) - 1 
+				# apakah kata sudah mirip >= 80% jika ya masukkan ke list pertanyaan
+				persentase = count/len(lisp[i]) * 100
+				if(persentase >= 80):
+					listPertanyaan.append(lisp[i])
+
+		# keluarkan jawaban dari bot
+		if(len(listPertanyaan) == 0):
+			# print("Can you ask other question ?")
+			result = "Can you ask another question ?"
+		else:
+			# if(len(listPertanyaan) == 1):
+				# print(k[listPertanyaan[0]])
+			result = kamus[listPertanyaan[0]]
+			# # lebih dari 1 yang mirip keluarkan jawaban random dari listPertanyaan yang terpilih
+			# else:
+			# 	idx = random.randint(0, len(listPertanyaan))
+			# 	print("xesbot : " + kamus[listPertanyaan[idx]])
 	else:
-		# if(len(listPertanyaan) == 1):
-			# print(k[listPertanyaan[0]])
-		result = kamus[listPertanyaan[0]]
-		# # lebih dari 1 yang mirip keluarkan jawaban random dari listPertanyaan yang terpilih
-		# else:
-		# 	idx = random.randint(0, len(listPertanyaan))
-		# 	print("xesbot : " + kamus[listPertanyaan[idx]])
+		result = "Please, insert a question !"
 
 	return render_template('html/kmp.html', answer=result)
 
@@ -85,26 +87,29 @@ def getkmp():
 @app.route('/bm', methods=['POST'])
 def getbm():
 	pertanyaan = sanitize(request.form['text']).lower()
-	result = -1
-	i = 0
-	while (i < len(lisp) and result < 0):
-		result = BM(lisp[i], pertanyaan)
-		i += 1
-	if (result < 0):
-		result = wordsBM(lisp,pertanyaan)
-		if (len(result)>0):
-			pertanyaan = result[0]
-			i = 0
-			result = -1
-			while (i < len(lisp) and result < 0):
-				result = BM(lisp[i],pertanyaan)
-				i += 1
-			result = kamus[i]
+	if (len(pertanyaan) != 0):
+		result = -1
+		i = 0
+		while (i < len(lisp) and result < 0):
+			result = BM(lisp[i], pertanyaan)
+			i += 1
+		if (result < 0):
+			result = wordsBM(lisp,pertanyaan)
+			if (len(result)>0):
+				pertanyaan = result[0]
+				i = 0
+				result = -1
+				while (i < len(lisp) and result < 0):
+					result = BM(lisp[i],pertanyaan)
+					i += 1
+				result = kamus[lisp[i-1]]
+			else:
+				# print("Can you ask other question ?")
+				result = "Can you ask another question ?"
 		else:
-			# print("Can you ask other question ?")
-			result = "Can you ask another question?"
+			result = kamus[lisp[i-1]]
 	else:
-		result = kamus[i]
+		result = "Please, insert a question !"
 
 	return render_template('html/bm.html', answer=result)
 
@@ -113,103 +118,22 @@ def getbm():
 def getregex():
 	pertanyaan = sanitize(request.form['text']).lower()
 	# pertanyaan = request.form['text']
-	result = []
-	i = 0
-	while(i < len(lisp) and not result): #periksa apakah pertanyaan
-		result = REGEX(lisp[i], pertanyaan)
-		i += 1
+	if (len(pertanyaan) != 0):
+		result = []
+		i = 0
+		while(i < len(lisp) and not result): #periksa apakah pertanyaan
+			result = REGEX(lisp[i], pertanyaan)
+			i += 1
 
-	if(result):
-		result = kamus[''.join(result)]
+		if(result):
+			result = kamus[''.join(result) + '?']
+		else:
+			result = "Can you ask another question ?"
 	else:
-		result = "Can you ask another question?"
-
+		result = "Please, insert a question !"
 
 	return render_template('html/regex.html', answer=result)
 
 
 if(__name__ == '__main__'):
 	app.run(debug=True)
-# #terima input user dan bersihkan dari dari stop word dan 
-# #ganti kata kata di dalamnya dengan sinonim yang terdapat 
-# #dalam database
-# pertanyaan = sanitize(raw_input())
-
-# #baca jenis algoritma string matching yang diinginkan user
-# algo = raw_input()
-
-# if(algo == "KMP"):
-# 	# cek keseluruhan kalimat terlebih dahulu
-# 	for i in range(len(lisp)):
-# 		idx = KMP.KMP(lisp[i], pertanyaan)
-# 		if(idx != -1):
-# 			listPertanyaan.append(lisp[i])
-
-# 	# cek kecocokan berdasarkan kata per kata dari input user jika tidak ada kata yang sama persis
-# 	if(len(listPertanyaan) == 0):
-# 		kalimat = pertanyaan.split(' ')
-# 		prevIdx = -1
-# 		for i in range(len(lisp)):
-# 			for kata in kalimat:
-# 				idx = KMP.KMP(lisp[i], kata)
-# 				if (idx > prevIdx):
-# 					listKataSama.append(kata)
-# 					prevIdx = idx
-
-# 			# hitung jumlah kata yang sama
-# 			count = 0
-# 			for kata in listKataSama:
-# 				count += len(kata)
-			
-# 			count += len(kalimat) - 1 
-# 			# apakah kata sudah mirip >= 80% jika ya masukkan ke list pertanyaan
-# 			persentase = count/len(p[i]) * 100
-# 			if(persentase >= 80):
-# 				listPertanyaan.append(p[i])
-
-# 	# keluarkan jawaban dari bot
-# 	if(len(listPertanyaan) == 0):
-# 		print("Can you ask another question ?")
-# 	else:
-# 		if(len(listPertanyaan) == 1):
-# 			print(k[listPertanyaan[0]])
-# 		# lebih dari 1 yang mirip keluarkan jawaban random dari listPertanyaan yang terpilih
-# 		else:
-# 			idx = random.randint(0, len(listPertanyaan))
-# 			print("xesbot : " + k[listPertanyaan[idx]])
-
-# elif(algo == "BM"):
-# 	result = -1
-# 	i = 0
-# 	while (i < len(lisp) and result < 0):
-# 		result = BM(lisp[i], pertanyaan)
-# 		i += 1
-# 	if (result < 0):
-# 		result = WordsBM(lisp,pertanyaan)
-# 		if (len(result)>0):
-# 			pertanyaan = result[0]
-# 			i = 0
-# 			result = -1
-# 			while (i < len(lisp) and result < 0):
-# 				result = BM(lisp[i],pertanyaan)
-# 				i += 1
-# 			result = kamus[i]
-# 		else:
-# 			print("Can you ask another question ?")
-# 	else:
-# 		result = kamus[i]
-
-# elif(algo == "REGEX"):
-# 	result = []
-# 	i = 0
-# 	while(!result): #periksa apakah pertanyaan
-# 		result = REGEX(pertanyaan, lisp)
-# 		i += 1
-
-# 	if(result):
-# 		result = kamus[result]
-# 	else:
-# 		print "dude what"
-
-# else:
-# 	print "Maaf algoritma tidak tersedia"
